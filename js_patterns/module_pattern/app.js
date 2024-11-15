70ca354070bd45b683956e8c1e6c64ef1a9b05c4
Gerardo Ramirez <gerardoramirez@users.noreply.github.com>
2024-11-14 19:52:18 -0800

    adding the latest updates to the sample code

---
+++ js_patterns/module_pattern/app.js
@@ -0,0 +1,12 @@
+// Import module from path
+import testModule from './testModule.js';
+
+// Increment our counter
+testModule.incrementCounter();
+
+// Check the counter value and reset
+// Outputs: counter value prior to reset: 1
+testModule.resetCounter();
+
+
+console.log("Hello, World!");
---
+++ js_patterns/module_pattern/testModuleTester.js
@@ -0,0 +1,9 @@
+// Import module from path
+import testModule from './testModule';
+
+// Increment our counter
+testModule.incrementCounter();
+
+// Check the counter value and reset
+// Outputs: counter value prior to reset: 1
+testModule.resetCounter();
