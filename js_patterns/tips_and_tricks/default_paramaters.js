52879f8a87f05d55c15f28df28046ea5d05130b4
Gerardo Ramirez <gerardoramirez@users.noreply.github.com>
2024-11-27 20:17:08 -0800

    Adding details from JS object creation.

---
+++ js_patterns/tips_and_tricks/default_paramaters.js
@@ -0,0 +1,16 @@
+/**
+ * Default parameters allow you to set default values for function arguments. This ensures that your function behaves predictably 
+ * when called without certain parameters.
+ * 
+ * In this example, the greet function uses a default value of "Guest" for the name parameter. If no argument is passed, it falls 
+ * back to the default. This is particularly useful for handling optional inputs, reducing the need for manual checks or fallback 
+ * logic inside the function.
+ * 
+ */
+
+function greet(name = "Guest") {
+    return `Hello, ${name}!`;
+  }
+  
+  console.log(greet()); // Output: Hello, Guest!
+  console.log(greet("Alice")); // Output: Hello, Alice!
---
+++ js_patterns/tips_and_tricks/optional_chaining.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Optional chaining ?. allows you to access deeply nested properties without worrying about 
+ * errors if a property doesn’t exist. Instead of throwing an error, it returns undefined.
+ * 
+ * Traditional property access would require checks like if (user && user.profile). Optional 
+ * chaining removes this hassle, ensuring your code doesn’t break if an intermediate property 
+ * (settings in this case) is undefined or null. This makes it easier to work with unpredictable 
+ * or incomplete data, especially from APIs.
+ * 
+ */
+
+
+const user = { profile: { email: "user@example.com" } };
+
+console.log(user?.profile?.email); // Output: user@example.com
+console.log(user?.settings?.theme); // Output: undefined (no error)
